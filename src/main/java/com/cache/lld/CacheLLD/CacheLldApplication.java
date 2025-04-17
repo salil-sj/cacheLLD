b05a68296894dcf69e4249a255dc06e1318b8e81
saliljoshi <salilsj1619@gmail.com>
2025-04-17 14:30:49 +0530

    Project Setup

---
+++ src/main/java/com/cache/lld/CacheLLD/CacheLldApplication.java
@@ -0,0 +1,13 @@
+package com.cache.lld.CacheLLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheLldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheLldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cache/lld/CacheLLD/CacheLldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cache.lld.CacheLLD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheLldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
